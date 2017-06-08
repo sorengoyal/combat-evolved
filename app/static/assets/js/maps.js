4a41136a882d6e7944c2111765e441fcbb77a5bc
Soren Goyal <sogoyal@CHIC02QV06NG8WN.sea.corp.expecn.com>
2017-06-07 21:25:40 -0500

    Cleaned up files, added polygon drawing support

--- app/static/assets/js/maps.js
+++ app/static/assets/js/maps.js
@@ -110,33 +110,11 @@ function initMap() {
     //Constructor: Creates a new map. Only zoom and corrdinates a required
     map = new google.maps.Map(document.getElementById('map'),{
             center: {lat:40.7413549, lng: -73.9980244},
-            zoom: 20,
+            zoom: 13,
             styles: style
     });
-    debugger;
+    
     var center = {lat:40.7413549, lng: -73.9980244};
-    var locations = [
-            {title: "Neil Ave Apartments", location:{lat:40.7413549, lng:-73.998024}},
-            {title: "Top Right Kill", location:{lat:40.7413559, lng:-73.998034}},
-            {title: "Open Heart Surgery", location:{lat:40.7413539, lng:-73.998014}},
-            {title: "Kill Bill Vol 2", location:{lat:40.7413539, lng:-73.998034}},
-            {title: "Ninety Percenty Kill Rate", location:{lat:40.7413559, lng:-73.998014}}
-            ];
-    var infoWindow = new google.maps.InfoWindow();
-    var markers = [];
-    for(var i = 0; i < locations.length; i++){
-        var marker = new google.maps.Marker({
-        position: locations[i].location,
-        map: map,
-        title: locations[i].title,
-        animation: google.maps.Animation.DROP,
-        id: i
-        });
-        marker.addListener('click', function(){
-            populateInfoWindow(this, infoWindow);
-        });
-        markers.push(marker);
-    }
     
     
     drawingManager = new google.maps.drawing.DrawingManager({
@@ -149,11 +127,8 @@ function initMap() {
             ]
         }
     });
-    
-    document.getElementById('toggle-drawing').addEventListener('click', function() {
-          toggleDrawing(drawingManager);
-        });
-    var path = null;
+    drawingManager.setMap(map)
+
     drawingManager.addListener('overlaycomplete', function(event) {
           // First, check if there is an existing polygon.
           // If there is, get rid of it and remove the markers
@@ -166,11 +141,8 @@ function initMap() {
           // Creating a new editable polygon from the overlay.
           polygon = event.overlay;
           polygon.setEditable(false);
-          // Searching within the polygon.
           path = polygon.getPath().b;
-          for(var i = 0; i < path.length; i++){
-            console.log(path[i].lat() + ',' + path[i].lng());
-            }
+          sendPolygon(path);
         });
     
 }
@@ -186,17 +158,22 @@ function populateInfoWindow(marker, infoWindow){
     }        
 }
 
-function toggleDrawing(drawingManager) {
-        if (drawingManager.map) {
-          drawingManager.setMap(null);
-          // In case the user drew anything, get rid of the polygon
-          if (polygon !== null) {
-            polygon.setMap(null);
-          }
-        } else {
-          drawingManager.setMap(map);
-        }
-      }
+function sendPolygon(polygon) {
+    var xhr = new XMLHttpRequest();
+    var url = window.location.origin + "/polygon";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    /*
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+        var json =  JSON.parse(xhr.responseText);
+            console.log(json.polygon );
+            }
+    }; */
+    var data = JSON.stringify({"polygon": polygon});
+    console.log(data)
+    xhr.send(data);
+}
 
 
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html', key = 'AIzaSyCdlOuxf56nB-hgN35Jpvk7qm7px-8wCPA')
+    return render_template('base.html', key = 'AIzaSyCdlOuxf56nB-hgN35Jpvk7qm7px-8wCPA')
+
+@app.route('/polygon', methods=['POST'])
+def receive():
+    print(request.get_json())
+    return render_template('base.html', key = 'AIzaSyCdlOuxf56nB-hgN35Jpvk7qm7px-8wCPA')
